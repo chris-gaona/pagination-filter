938ef51cb1825e2837a7b60062cf4719d2e768e8
Chris Gaona <thebeloved88@sbcglobal.net>
2016-04-29 13:23:26 -0700

    Programmatically adds the appropriate number of pagination links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,39 @@
-(function() {
+$(function() {
   'use strict';
 
+  //define search variable to hold search html
   var search = '<div class="student-search"><input placeholder="Search for students..."><button>Search</button></div>';
 
-  var pagination = '<div class="pagination"><ul><li><a class="active" href="#">1</a></li><li><a href="#">2</a></li><li><a href="#">3</a></li><li><a href="#">4</a></li><li><a href="#">5</a></li></ul></div>'
+  //define pagination variable to hold pagination
+  //button html
+  var pagination = '<div class="pagination"><ul></ul></div>'
 
+  //use jquery to append search bar to page-header class
   $('.page-header').append(search);
 
+  //use jquery to append pagination buttons to
+  //page class
   $('.page').append(pagination);
-})();
+
+  //Since only 10 students should be shown at a time, your programming needs to calculate the number of pages needed and add the appropriate number of links to the bottom of the page.
+  function numberPageLinks() {
+    var linksPerPage = 10;
+    var totalStudents = $('.student-list li').length;
+    var totalLinks = Math.floor(totalStudents/linksPerPage);
+
+    var links = 0;
+    var linkNumber = 1;
+
+    while (links < totalLinks) {
+      $('.pagination ul').append('<li><a href="#">' + linkNumber++ + '</a></li>');
+      links++;
+    }
+  }
+  numberPageLinks();
+
+  //Hide all but the first 10 students when the page loads.
+
+  //When a user clicks on “2” in the pagination, students 11 through 20 are shown. When a user clicks “3”, students 21 through 30 are shown. And so on. When “6” is clicked 51 through 55 should be shown.
+
+
+});
